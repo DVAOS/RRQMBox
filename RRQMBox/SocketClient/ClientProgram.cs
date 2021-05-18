88f5c0724f3a3eec019a9bc4957ee36823ff3c07
御风踏青岚 <1751615839@qq.com>
2021-05-18 17:25:52 +0800

    更新Demo

--- RRQMBox/SocketClient/ClientProgram.cs
+++ RRQMBox/SocketClient/ClientProgram.cs
@@ -12,6 +12,7 @@
 using System;
 using System.Text;
 using System.Threading;
+using RRQMCore.ByteManager;
 using RRQMSocket;
 
 namespace Demo.TestTcpClient
@@ -21,42 +22,75 @@ namespace Demo.TestTcpClient
         private static void Main(string[] args)
         {
             Console.ReadKey();
-
-            for (int i = 0; i < 1; i++)
-            {
-                ThreadPool.QueueUserWorkItem((object o) =>
-                {
-                    MyTcpClientTest();
-                });
-            }
-
+            TestTcpClient();
+            TestTokenTcpClient();
             Console.ReadKey();
         }
 
-        private static TimeSpan time;
         private static int count;
 
-        private static void MyTcpClientTest()
+        private static void TestTokenTcpClient()
         {
-            MyTcpClient client = new MyTcpClient();
+            TokenTcpClient client = new TokenTcpClient();
 
-            client.Connect(new IPHost("127.0.0.1:7789"));
+            //属性
+            client.VerifyToken = "ABC";//设置验证口令
+            client.BufferLength = 1024;//设置缓存池大小，该数值在框架中经常用于申请ByteBlock，所以该值会影响内存池效率。
+            client.Logger = new Log();//设置内部日志记录器，默认日志是控制台输出。
+            client.DataHandlingAdapter = new NormalDataHandlingAdapter();//数据处理适配器，可用于处理粘包、解析对象。
+
+            //事件
+            client.ConnectedService += Client_ConnectedService;
+            client.DisconnectedService += Client_DisconnectedService;
+            client.OnReceived += Client_OnReceived;
+
+            //方法
+            client.Connect(new IPHost("127.0.0.1:7791"));//连接
             Console.WriteLine("连接成功");
-            byte[] datas = Encoding.UTF8.GetBytes("若汝棋茗");
+            client.Send(Encoding.UTF8.GetBytes("若汝棋茗"));//发送数据
+            Console.WriteLine("发送成功");
+        }
 
-            TimeSpan timeSpan = RRQMCore.Diagnostics.TimeMeasurer.Run(() =>
+        private static void TestTcpClient()
+        {
+            TcpClient client = new TcpClient();
+
+            //属性
+            client.BufferLength = 1024;//设置缓存池大小，该数值在框架中经常用于申请ByteBlock，所以该值会影响内存池效率。
+            client.Logger = new Log();//设置内部日志记录器，默认日志是控制台输出。
+            client.DataHandlingAdapter = new NormalDataHandlingAdapter();//数据处理适配器，可用于处理粘包、解析对象。
+
+            //事件
+            client.ConnectedService += Client_ConnectedService;
+            client.DisconnectedService += Client_DisconnectedService;
+            client.OnReceived += Client_OnReceived;
+
+            //方法
+            client.Connect(new IPHost("127.0.0.1:7789"));//连接
+            Console.WriteLine("连接成功");
+            client.Send(Encoding.UTF8.GetBytes("若汝棋茗"));//发送数据
+            Console.WriteLine("发送成功");
+
+        }
+
+        private static void Client_OnReceived(TcpClient arg1, ByteBlock byteBlock, object arg3)
+        {
+            count++;
+            if (count % 100 == 0)
             {
-                for (int i = 0; i < 1; i++)
-                {
-                    client.Send(datas);
-                }
-            });
-            lock (typeof(ClientProgram))
-            {
-                count++;
-                time += timeSpan;
-                Console.WriteLine(TimeSpan.FromTicks(time.Ticks / count));
+                string mes = Encoding.UTF8.GetString(byteBlock.Buffer, 0, (int)byteBlock.Position);
+                Console.WriteLine($"已接收到信息：{mes},第{count}条");
             }
+        }
+
+        private static void Client_DisconnectedService(object sender, MesEventArgs e)
+        {
+            Console.WriteLine($"已断开连接");
+        }
+
+        private static void Client_ConnectedService(object sender, MesEventArgs e)
+        {
+            Console.WriteLine($"已成功连接");
         }
     }
 }
---
+++ RRQMBox/SocketStressTesting/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using RRQMCore.ByteManager;
+using RRQMSocket;
+
+namespace SocketStressTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.测试连接");
+            Console.WriteLine("2.测试使用固定包头发送");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    {
+                        TestConnect();
+                        break;
+                    }
+                case "2":
+                    {
+                        TestFixedHeaderSend();
+                        break;
+                    }
+                default:
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 测试连接
+        /// </summary>
+        static void TestConnect()
+        {
+            Task.Run(() =>
+            {
+                int count = 1000;
+                TimeSpan timeSpan = RRQMCore.Diagnostics.TimeMeasurer.Run(() =>
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        Socket tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        tcpClient.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7789));
+                        tcpClient.Dispose();
+                    }
+                });
+                Console.WriteLine($"Socket连接{count}次，用时{timeSpan}");
+            });
+
+            Task.Run(() =>
+            {
+                int count = 1000;
+                TimeSpan timeSpan = RRQMCore.Diagnostics.TimeMeasurer.Run(() =>
+                  {
+                      for (int i = 0; i < count; i++)
+                      {
+                          RRQMSocket.TcpClient tcpClient = new RRQMSocket.TcpClient();
+                          tcpClient.Connect(new IPHost("127.0.0.1:7789"));
+                          tcpClient.Dispose();
+                      }
+                  });
+                Console.WriteLine($"普通TCP连接{count}次，用时{timeSpan}");
+            });
+
+            Task.Run(() =>
+            {
+                int count = 1000;
+                TimeSpan timeSpan = RRQMCore.Diagnostics.TimeMeasurer.Run(() =>
+                  {
+                      for (int i = 0; i < count; i++)
+                      {
+                          TokenTcpClient client = new TokenTcpClient();
+                          client.VerifyToken = "ABC";
+                          client.Connect(new IPHost("127.0.0.1:7791"));
+                          client.Dispose();
+                      }
+                  });
+                Console.WriteLine($"TokenTCP连接{count}次，用时{timeSpan}");
+            });
+        }
+
+        /// <summary>
+        /// 测试使用固定包头发送
+        /// </summary>
+        static void TestFixedHeaderSend()
+        {
+            RRQMSocket.TcpClient tcpClient = new RRQMSocket.TcpClient();
+            tcpClient.OnReceived += TcpClient_OnReceived;
+            tcpClient.DataHandlingAdapter = new FixedHeaderDataHandlingAdapter();
+            tcpClient.Connect(new IPHost("127.0.0.1:7789"));
+
+            byte[] data = Encoding.UTF8.GetBytes("RRQM");
+            Task.Run(() =>
+            {
+                int count = 1000;
+                TimeSpan timeSpan = RRQMCore.Diagnostics.TimeMeasurer.Run(() =>
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        tcpClient.Send(data);
+                    }
+                });
+                Console.WriteLine($"TCP发送{count}次，用时{timeSpan}");
+            });
+        }
+
+        private static int count;
+        private static void TcpClient_OnReceived(RRQMSocket.TcpClient arg1, ByteBlock byteBlock, object arg3)
+        {
+            count++;
+            if (count % 1 == 0)
+            {
+                string mes = Encoding.UTF8.GetString(byteBlock.Buffer, 0, (int)byteBlock.Length);
+                Console.WriteLine($"已接收到信息：{mes},第{count}条");
+            }
+        }
+    }
+}
