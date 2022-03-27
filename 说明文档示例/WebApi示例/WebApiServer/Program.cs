53528395372664b2dc5fd57baf632b87c916ac88
若汝棋茗 <1751615839@qq.com>
2022-03-27 19:11:40 +0800

    新增

---
+++ 说明文档示例/WebApi示例/WebApiServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using RRQMSocket.RPC;
+using RRQMSocket.RPC.WebApi;
+using System;
+
+namespace WebApiServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    [Route("[controller]/[action]")]
+    public class ServerController : ServerProvider
+    {
+        [HttpGet]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        [HttpPost]
+        public int TestPost(MyClass myClass)
+        {
+            return myClass.A + myClass.B;
+        }
+    }
+
+    public class MyClass
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+    }
+}
