d1e2ed5bce30bb6084e6880338f0df55945e535d
若汝棋茗 <505554090@qq.com>
2022-05-31 10:34:49 +0800

    T

--- 说明文档示例/Tcp示例/ServiceApp/Program.cs
+++ 说明文档示例/Tcp示例/ServiceApp/Program.cs
@@ -12,7 +12,7 @@ namespace ServiceApp
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main() 
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
