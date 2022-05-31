2b7f0d2d7a85cd7d94c482473bb0d773a0614843
若汝棋茗 <5055540901@qq.com>
2022-05-31 15:48:36 +0800

    添加Consul集群示例

---
+++ 说明文档示例/使用Consul集群服务发现、治理/ServiceConsoleApp/Program.cs
@@ -0,0 +1,170 @@
+﻿using Consul;
+using RRQMCore.Log;
+using RRQMSocket;
+using RRQMSocket.Http;
+using RRQMSocket.RPC;
+using RRQMSocket.RPC.JsonRpc;
+using RRQMSocket.RPC.TouchRpc;
+using RRQMSocket.RPC.WebApi;
+using RRQMSocket.RPC.XmlRpc;
+using RRQMSocket.WebSocket;
+using RRQMSocket.WebSocket.Plugins;
+using System;
+
+namespace ServiceConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //因为HttpTouchRpcService是企业版功能，所以需要启用限时测试。
+                //当然该功能可以用HttpService和TcpTouchRpcService组合实现，区别就是需要监听两个端口。
+                RRQMCore.Enterprise.ForTest();
+            }
+            catch
+            {
+
+            }
+           
+
+            //此处直接建立HttpTouchRpcService。
+            //此组件包含Http所有功能，可以承载JsonRpc、XmlRpc、WebSocket、TouchRpc等等。
+            var service = new RRQMConfig()
+                .UsePlugin()
+                .SetBufferLength(1024 * 64)
+                .SetThreadCount(50)
+                .SetReceiveType(ReceiveType.Auto)
+                .SetListenIPHosts(new IPHost[] { new IPHost(7789) })
+                .SetLogger<ConsoleLogger>()
+                .BuildWithHttpTouchRpcService();
+
+            service.Logger.Message("Http服务器已启动");
+
+            service.AddPlugin<WebSocketServerPlugin>().//添加WebSocket功能
+                    SetWSUrl("/ws");
+            service.Logger.Message("WS插件已加载，使用 ws://127.0.0.1:7789/ws 连接");
+
+            service.AddPlugin<MyWebSocketPlug>();//添加WebSocket业务数据接收插件
+            service.AddPlugin<MyWebSocketCommand>();//添加WebSocket快捷实现，常规WS客户端发送文本“Add 10 20”即可得到30。
+            service.Logger.Message("WS命令行插件已加载，使用WS发送文本“Add 10 20”获取答案");
+
+            IRpcParser jsonRpcParser = service.AddPlugin<JsonRpcParserPlugin>()
+                 .SetJsonRpcUrl("/jsonrpc");
+            service.Logger.Message("jsonrpc插件已加载，使用 Http://127.0.0.1:7789/jsonrpc +JsonRpc规范调用");
+
+            IRpcParser xmlRpcParser = service.AddPlugin<XmlRpcParserPlugin>()
+                .SetXmlRpcUrl("/xmlrpc");
+            service.Logger.Message("jsonrpc插件已加载，使用 Http://127.0.0.1:7789/xmlrpc +XmlRpc规范调用");
+
+            IRpcParser webApiParser = service.AddPlugin<WebApiParserPlugin>();
+            service.Logger.Message("WebApi插件已加载");
+
+            RpcStore rpcStore = new RpcStore();
+            rpcStore.AddRpcParser("httpTouchRpcService", service);
+            rpcStore.AddRpcParser("jsonRpcParser", jsonRpcParser);
+            rpcStore.AddRpcParser("xmlRpcParser", xmlRpcParser);
+            rpcStore.AddRpcParser("webApiParser", webApiParser);
+            rpcStore.RegisterServer<MyServer>();
+            service.Logger.Message("RPC注册完成。");
+
+            rpcStore.ShareProxy(new IPHost(8848));
+            rpcStore.ProxyUrl = "/proxy";
+            service.Logger.Message("RPC代理文件已分享，使用 Http://127.0.0.1:8848/proxy?proxy=all 获取");
+
+            RegisterConsul();
+            service.Logger.Message("Consul已成功注册");
+
+            while (Console.ReadKey().Key!= ConsoleKey.Escape)
+            {
+                Console.WriteLine("按ESC键退出。");
+            }
+        }
+
+        /// <summary>
+        /// 注册Consul，使用该功能时，请先了解Consul，然后配置基本如下。
+        /// </summary>
+        public static void RegisterConsul()
+        {
+            var consulClient = new ConsulClient(p => { p.Address = new Uri($"http://127.0.0.1:8500"); });//请求注册的 Consul 地址
+                                                                                                         //这里的这个ip 就是本机的ip，这个端口8500 这个是默认注册服务端口 
+            var httpCheck = new AgentServiceCheck()
+            {
+                DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(5),//服务启动多久后注册
+                Interval = TimeSpan.FromSeconds(10),//间隔固定的时间访问一次，https://127.0.0.1:7789/api/Health
+                HTTP = $"http://127.0.0.1:7789/api/Health",//健康检查地址
+                Timeout = TimeSpan.FromSeconds(5)
+            };
+
+            var registration = new AgentServiceRegistration()
+            {
+                Checks = new[] { httpCheck },
+                ID = Guid.NewGuid().ToString(),
+                Name = "RRQMService",
+                Address = "127.0.0.1",
+                Port = 7789
+            };
+
+            consulClient.Agent.ServiceRegister(registration).Wait();//注册服务 
+
+            //consulClient.Agent.ServiceDeregister(registration.ID).Wait();//registration.ID是guid
+            //当服务停止时需要取消服务注册，不然，下次启动服务时，会再注册一个服务。
+            //但是，如果该服务长期不启动，那consul会自动删除这个服务，大约2，3分钟就会删了 
+
+        }
+    }
+
+    class MyServer : ServerProvider
+    {
+        [WebApi(HttpMethodType.GET)]
+        [XmlRpc]
+        [JsonRpc]
+        [TouchRpc]
+        public string SayHello(string name)
+        {
+            return $"{name},RRQM says hello to you.";
+        }
+
+        [WebApi(HttpMethodType.GET,Route = "/api/health")]
+        public string Health()
+        {
+            return "ok";
+        }
+    }
+
+    /// <summary>
+    /// WS命令行执行
+    /// </summary>
+    class MyWebSocketCommand : WSCommandLinePlugin
+    {
+        public int AddCommand(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    /// <summary>
+    /// WS收到数据等业务。
+    /// </summary>
+    class MyWebSocketPlug : WebSocketPluginBase
+    {
+        protected override void OnHandshaked(ITcpClientBase client, HttpContextEventArgs e)
+        {
+            SocketClient socketClient = (SocketClient)client;
+
+            client.Logger.Message($"WS客户端连接，ID={socketClient.ID}，IPHost={client.IP}:{client.Port}");
+            base.OnHandshaked(client, e);
+        }
+
+        protected override void OnHandleWSDataFrame(ITcpClientBase client, WSDataFrameEventArgs e)
+        {
+            if (e.DataFrame.Opcode == WSDataType.Text)
+            {
+                client.Logger.Message($"WS Msg={e.DataFrame.ToText()}");
+            }
+
+            base.OnHandleWSDataFrame(client, e);
+        }
+    }
+}
